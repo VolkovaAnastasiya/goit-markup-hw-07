8591279f96ca97294b2f9216fb6e4e6296b5b8e6
VolkovaAnastasiya <volkova.anastasiya1986@gmail.com>
2021-08-24 21:53:01 +0300

    фиксит закрытие модалки

--- js/modal.js
+++ js/modal.js
@@ -1,7 +1,7 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal_close]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
   };
 
